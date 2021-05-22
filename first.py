98c6d8a62835d33dae215d29f3167013ccaeb635
Andrew Santhosh <andrewsanthosh264@gmail.com>
2021-05-23 01:57:21 +0530

    first.py: buttons

--- first.py
+++ first.py
@@ -7,18 +7,32 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setGeometry(200, 200, 300, 300)
-        self.setWindowTitle("Test Application")
+        self.setWindowTitle("YouTube Downloader")
         self.initUI()
         
     def initUI(self):
         self.b1 = QtWidgets.QPushButton(self)
-        self.b1.setText("Click")
+        self.b2 = QtWidgets.QPushButton(self)
+        self.textbox = QLineEdit(self)
+        self.textbox.move(15, 20)
+        self.textbox.resize(280,40)
+        self.b1.setText("MP4")
+        self.b1.move(110, 80)
+        self.b1.resize(80,40)
         self.b1.clicked.connect(self.b1click)
+        self.b2.setText("MP3")
+        self.b2.move(110, 120)
+        self.b2.resize(80,40)
+        self.b2.clicked.connect(self.b2click)
 
     def b1click(self):
+        self.b1.setText("OK !!!")
         # self.download()
-        self.update()
+        # self.update()
         
+    def b2click(self):
+        self.b2.setText("OK !!!")
+
     # def download(self):
     #     self.yt = YouTube(self.link)
     #     self.yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
